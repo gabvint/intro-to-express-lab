ec87beb44a70a5141c97848e054c89eb5ec4c832
gabtongol <geybtongol@gmail.com>
2024-07-22 19:04:48 -0400

    3 done

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+/// data 
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
 // import express 
 const express = require('express')
 const morgan = require('morgan')
@@ -23,7 +30,20 @@ app.get('/roll/:number', (req, res) => {
     }
 });
 
+app.get('/collectibles/:number', (req, res) => {
+    
+    let num = req.params.number
+
+    if (num < collectibles.length){
+        res.send(`<h2>So, you want the ${collectibles[num].name}? For ${collectibles[num].price}, it can be yours!</h2>`)
+    } else {
+        res.send(`<h2>This item is not yet in stock. Check back soon!</h2>`)
+    }
+
+
+});
 //listening on port 
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
