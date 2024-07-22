0fb0147f4721291cc6e9385799587ca175558311
gabtongol <geybtongol@gmail.com>
2024-07-22 18:56:22 -0400

    2 done

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ app.get('/greetings/:name', (req, res) => {
     res.send(`<h2>What a delight it is to see you once more, ${req.params.name}</h2>`)
 });
 
+app.get('/roll/:number', (req, res) => {
 
+    let num = req.params.number
+    if (isNaN(num)){
+        res.send(`<h2>You must specify a number.</h2>`)
+    }else{
+        num = parseInt(Math.random() * num)
+    res.send(`<h2>You rolled a ${num}</h2>`)
+    }
+});
 
 //listening on port 
 
